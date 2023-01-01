edfa627fbc8d64ce3c2642711731a57e2ae223e2
Hell-botz <92094675+Hell-botz@users.noreply.github.com>
2023-01-01 23:08:57 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -908,7 +908,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         buttons = [[
                     InlineKeyboardButton('+ ᴀᴅᴅ ᴍᴇ ᴛᴏ yᴏᴜʀ ɢʀᴏᴜᴘ +', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
                 ],[
-                    InlineKeyboardButton('ʜᴇʟᴘ', callback_data='help"),
+                    InlineKeyboardButton('ʜᴇʟᴘ', callback_data='help'),
                     InlineKeyboardButton('ɪɴʟɪɴᴇ ꜱᴇᴀʀᴄʜ', switch_inline_query_current_chat='')
                 ],[
                     InlineKeyboardButton('ᴀʙᴏᴜᴛ', callback_data='about'),
